7d5f956184ec77589a479c44bcdee1195d2bb629
tienpmath <designdalat@gmail.com>
2024-05-17 16:46:21 +0700

    footer1

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -20,9 +20,7 @@ const Map = () => {
               src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3909.0364773475917!2d107.81332017505332!3d11.549241088650447!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTHCsDMyJzU3LjMiTiAxMDfCsDQ4JzU3LjIiRQ!5e0!3m2!1svi!2s!4v1715935454898!5m2!1svi!2s"
               width="100%"
               height="450"
-              allowfullscreen="true"
               loading="lazy"
-              referrerpolicy="no-referrer-when-downgrade"
             ></iframe>
           </div>
           <div className="banner-content absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center">
@@ -47,9 +45,7 @@ const Map = () => {
               src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3903.4310348406216!2d108.43651017505951!3d11.944630588284273!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTHCsDU2JzQwLjciTiAxMDjCsDI2JzIwLjciRQ!5e0!3m2!1svi!2s!4v1715936153272!5m2!1svi!2s"
               width="100%"
               height="450"
-              allowfullscreen="true"
               loading="lazy"
-              referrerpolicy="no-referrer-when-downgrade"
             ></iframe>
           </div>
           <div className="banner-content absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center">
